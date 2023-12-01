7d8eab36ccc1a753587931a3ca13ee963eef2fac
mikegorski <mike.gorski96@gmail.com>
2023-12-02 00:17:01 +0100

    day 01 refactor

--- 2023/day01/main.py
+++ 2023/day01/main.py
@@ -1,38 +1,43 @@
-FILE = "input.txt"
+from typing import Iterator
+
+
+FILENAME = "input.txt"
 DIGITS = "0123456789"
 SPELLED = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 VALID = list(DIGITS) + SPELLED
 
 
-def part1():
+def gen_rows(filename: str) -> Iterator:
+    with open(filename, "r") as file:
+        for line in file:
+            yield line.rstrip()
+
+
+def part1(filename: str) -> int:
     total = 0
-    with open(FILE, "r") as file:
-        for row in file:
-            chars = row.rstrip()
-            first_digit = next((char for char in chars if char in DIGITS), None)
-            last_digit = next((char for char in chars[::-1] if char in DIGITS), None)
-            total += int(first_digit + last_digit)
+    for row in gen_rows(filename):
+        first_digit = next((char for char in row if char in DIGITS), None)
+        last_digit = next((char for char in row[::-1] if char in DIGITS), None)
+        total += int(first_digit + last_digit)
     return total
 
 
-def part2():
+def part2(filename: str) -> int:
     total = 0
-    with open(FILE, "r") as file:
-        for row in file:
-            chars = row.rstrip()
-            ind_left = {chars.find(d): d for d in VALID if chars.find(d) != -1}
-            ind_right = {chars.rfind(d): d for d in VALID if chars.rfind(d) != -1}
-            indices = ind_left | ind_right
-            first_digit = indices[min(indices.keys())]
-            last_digit = indices[max(indices.keys())]
-            if first_digit in SPELLED:
-                first_digit = SPELLED.index(first_digit) + 1
-            if last_digit in SPELLED:
-                last_digit = SPELLED.index(last_digit) + 1
-            total += int(first_digit) * 10 + int(last_digit)
+    for row in gen_rows(filename):
+        ind_left = {row.find(d): d for d in VALID if row.find(d) != -1}
+        ind_right = {row.rfind(d): d for d in VALID if row.rfind(d) != -1}
+        indices = ind_left | ind_right
+        first_digit = indices[min(indices.keys())]
+        last_digit = indices[max(indices.keys())]
+        if first_digit in SPELLED:
+            first_digit = SPELLED.index(first_digit) + 1
+        if last_digit in SPELLED:
+            last_digit = SPELLED.index(last_digit) + 1
+        total += int(first_digit) * 10 + int(last_digit)
     return total
 
 
 if __name__ == '__main__':
-    print(f"Part 1: {part1()}")
-    print(f"Part 2: {part2()}")
+    print(f"Part 1: {part1(FILENAME)}")
+    print(f"Part 2: {part2(FILENAME)}")
