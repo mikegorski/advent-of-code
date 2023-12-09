1793539de68e6dd4bbeeebefc56744951f81c33a
mikegorski <mike.gorski96@gmail.com>
2023-12-09 10:48:56 +0100

    day08 refactor DRY

--- 2023/day08/day08.py
+++ 2023/day08/day08.py
@@ -23,12 +23,12 @@ def parse_input() -> tuple[list[int], dict[str, tuple[str, str]]]:
     return instructions, mapping
 
 
-def navigate(start: str, instructions: list[int], mapping: dict[str, tuple[str, str]]) -> int:
+def navigate(start: str, end: str, instructions: list[int], mapping: dict[str, tuple[str, str]]) -> int:
     nxt = start
     steps = 0
     dir_iter = itertools.cycle(instructions)
 
-    while not nxt.endswith("Z"):
+    while not nxt.endswith(end):
         direction = next(dir_iter)
         nxt = mapping[nxt][direction]
         steps += 1
@@ -37,25 +37,13 @@ def navigate(start: str, instructions: list[int], mapping: dict[str, tuple[str, 
 
 def part1() -> int:
     instructions, mapping = parse_input()
-    nxt = "AAA"
-    steps = 0
-    dir_iter = itertools.cycle(instructions)
-
-    while nxt != "ZZZ":
-        direction = next(dir_iter)
-        nxt = mapping[nxt][direction]
-        steps += 1
-    return steps
+    return navigate("AAA", "ZZZ", instructions, mapping)
 
 
 def part2() -> int:
     instructions, mapping = parse_input()
     starting_points = [k for k in mapping if k.endswith("A")]
-    all_steps: list[int] = []
-    for start in starting_points:
-        steps = navigate(start, instructions, mapping)
-        all_steps.append(steps)
-    return math.lcm(*all_steps)
+    return math.lcm(*[navigate(start, "Z", instructions, mapping) for start in starting_points])
 
 
 if __name__ == "__main__":
