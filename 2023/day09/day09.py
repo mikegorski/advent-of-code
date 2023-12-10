67785833e5008c3506dae3203eb701843aeb9ee9
mikegorski <mike.gorski96@gmail.com>
2023-12-10 13:08:20 +0100

    day09 refactored DRY

--- 2023/day09/day09.py
+++ 2023/day09/day09.py
@@ -19,7 +19,7 @@ def get_diffs(vals: list[int]) -> tuple[list[int], bool]:
     return diffs, len(set(diffs)) == 1
 
 
-def forecast_fwd(vals: list[int]) -> int:
+def forecast(vals: list[int]) -> int:
     all_zero: bool = False
     next_val = vals[-1]
     while not all_zero:
@@ -28,21 +28,12 @@ def forecast_fwd(vals: list[int]) -> int:
     return next_val
 
 
-def forecast_back(vals: list[int]) -> int:
-    all_zero: bool = False
-    next_val = [vals[0]]
-    while not all_zero:
-        vals, all_zero = get_diffs(vals)
-        next_val.append(vals[0])
-    return sum(-n if i % 2 else n for i, n in enumerate(next_val))
-
-
 def part1() -> int:
-    return sum(forecast_fwd(row) for row in parse_rows())
+    return sum(forecast(row) for row in parse_rows())
 
 
 def part2() -> int:
-    return sum(forecast_back(row) for row in parse_rows())
+    return sum(forecast(list(reversed(row))) for row in parse_rows())
 
 
 if __name__ == "__main__":
